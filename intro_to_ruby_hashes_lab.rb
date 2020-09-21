aee78e4278ebc61a5918bd1d87013b80006537d4
Joy Evans <joyevans@me.com>
2020-09-21 21:59:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,9 @@ def monopoly_with_third_tier
   
   {:railroads => {
     :pieces => 4 ,
-    :rent_in_dollars => {{:one_piece_owned => 25},
-      {:two_pieces_owned => 50}
+    :rent_in_dollars => {
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50
     },
     :names => {}
     }
